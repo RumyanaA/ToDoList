df91a10ce24a78227a7930ff499ece9ba78c31a2
unknown <rumyana.atanasova97@gmail.com>
2021-08-19 11:57:26 +0300

    implemented general storage on front end

---
+++ src/Storage.js
@@ -0,0 +1,32 @@
+class Storage {
+    static categories=[];
+    static tasks=[];
+    static notifications=[];
+
+    static setItem(item, fieldName){
+        this[fieldName].push(item)
+
+    }
+
+    static getItem(key, value, fieldName ){
+
+        var result=this[fieldName].find(item => item[key]==value) // [] are to search the static properties and not the string
+        return result;
+
+    }
+  
+    static updateItem(key, value,keyToUpdate, updateValue, fieldName){
+        for(var i=0;i<this[fieldName].length; i++){
+            if(this[fieldName][i][key]==value){
+                this[fieldName][i][keyToUpdate]=updateValue;
+            }
+        }
+    }
+    static deleteItem(key, value, fieldName){
+        var index = this[fieldName].findIndex(item => item[key] == value);
+        if (index > -1) {
+            this[fieldName].splice(index, 1);
+          }
+}
+}
+export default Storage;
